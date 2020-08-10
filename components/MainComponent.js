02cbc750e776c19a4f1ef8b65682d0137486f39c
anishrajan25 <anishrajan2708@gmail.com>
2020-08-10 10:14:56 +0530

    1 stack navigation week 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import Dishdetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
     };
   }
 
   render() {
+
+    const MenuNavigator = createStackNavigator({
+        Menu: { screen: Menu },
+        Dishdetail: { screen: Dishdetail }
+    },
+    {
+        initialRouteName: 'Menu',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: "#512DA8"
+            },
+            headerTintColor: '#fff', // for icons
+            headerTitleStyle: {
+                color: "#fff"            
+            }
+        }
+    }
+    );
  
     return (
-        <Menu dishes={this.state.dishes} />
+        <View style={{flex:1}}>
+            <MenuNavigator />
+        </View>
     );
   }
 }
