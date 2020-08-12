a79f9549ae7ba3ab0a44398281d19fc5e5add0a8
anishrajan25 <anishrajan2708@gmail.com>
2020-08-12 22:33:03 +0530

    social sharing week 3

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, StyleSheet, FlatList, Button, Alert, Modal, PanResponder } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, FlatList, Button, Alert, Modal, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux'
 import { baseUrl } from "../shared/baseUrl";
@@ -79,6 +79,16 @@ function RenderDish(props) {
         }
     });
 
+    const shareDish = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: title + ': ' + message + ' ' + url,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        });
+    }
+
         if (dish != null) {
             return(
                 <Animatable.View animation='fadeInDown' duration={2000} delay={1000}
@@ -107,6 +117,14 @@ function RenderDish(props) {
                                 type='font-awesome'
                                 color='#512DAB'
                                 onPress={() => props.toggleModal() } 
+                            />
+                            <Icon 
+                                raised //shows it like a button
+                                reverse //icon of one color and surrounding of another
+                                name={'share'}
+                                type='font-awesome'
+                                color='#51D2AB'
+                                onPress={() => shareDish(dish.name, dish.description, baseUrl + dish.image) } 
                             />
                         </View>
                         <Modal
